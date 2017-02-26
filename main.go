4e36d507ca9c896b436d810f16c8fe0a1d2661c0
Luca Grulla <luca.grulla@gmail.com>
2017-02-26 21:48:34 +0000

    Add Makefile

--- main.go
+++ main.go
@@ -9,8 +9,6 @@ import (
 )
 
 var (
-	timeFormat = "2006-01-02T15:04:05"
-
 	tailCommand     = kingpin.Command("tail", "Tail a log group")
 	lsCommand       = kingpin.Command("ls", "show all log groups")
 	logGroupPattern = lsCommand.Arg("group", "the log group name").String()
@@ -19,10 +17,6 @@ var (
 	startTime       = tailCommand.Arg("start", "The start time").Default(time.Now().Format(timeutil.TimeFormat)).String()
 	streamName      = tailCommand.Arg("stream", "Stream name").String()
 )
-
-func formatTimestamp(ts int64) string {
-	return time.Unix(ts, 0).Format(timeFormat)
-}
 
 func main() {
 	kingpin.Version("0.0.1")
