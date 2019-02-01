7a260ecff542baa57b0ff666eb6641edf2ca34e4
Luca Grulla <luca.grulla@gmail.com>
2019-02-01 20:40:57 +0000

    refactor coordinator out in a seperate file

---
+++ coordinator.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"container/ring"
+	"log"
+	"sync"
+	"time"
+)
+
+type tailCoordinator struct {
+	targets *ring.Ring
+	sync.RWMutex
+	log *log.Logger
+}
+
+func (f *tailCoordinator) start(targets []chan<- time.Time) {
+	f.targets = ring.New(len(targets))
+	for i := 0; i < f.targets.Len(); i++ {
+		f.targets.Value = targets[i]
+		f.targets = f.targets.Next()
+	}
+
+	//AWS API accepts 5 reqs/sec for account
+	ticker := time.NewTicker(205 * time.Millisecond)
+	go func() {
+		for range ticker.C {
+			if f.targets == nil {
+				f.log.Println("coordinator: ring buffer is empty, exiting scheduler.")
+				return
+			}
+			f.Lock()
+			x := f.targets.Value.(chan<- time.Time)
+			x <- time.Now()
+			f.targets = f.targets.Next()
+			f.Unlock()
+		}
+	}()
+}
+
+func (f *tailCoordinator) remove(c chan<- time.Time) {
+	f.RLock()
+	initialLen := f.targets.Len()
+	f.RUnlock()
+
+	var visited int
+	f.Lock()
+	defer f.Unlock()
+
+	if f.targets.Len() == 1 {
+		f.targets = ring.New(0)
+		f.log.Println("coordinator: single node buffer: reset", f.targets.Len())
+
+		return
+	}
+
+	for visited = 0; visited < f.targets.Len(); visited++ {
+		if f.targets.Value == c {
+			targetChan := f.targets.Value.(chan<- time.Time)
+
+			f.targets = f.targets.Prev()
+			f.targets.Unlink(1)
+			close(targetChan)
+			f.log.Printf("coordinator: channel found and removed at index: %d\n", visited)
+
+			break
+		}
+		f.targets = f.targets.Next()
+	}
+
+	if f.targets.Len() < initialLen {
+		for i := 0; i < visited; i++ {
+			f.targets = f.targets.Prev()
+		}
+	}
+}
