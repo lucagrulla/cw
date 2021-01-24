af49ea73a3064867e0b4b24d17df8e39f0407e81
Luca Grulla <luca.grulla@gmail.com>
2021-01-24 18:21:41 +0000

    add completion files

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	timeFormat = "2006-01-02T15:04:05"
-	version    = "4.0.0"
+	version    = "4.0.2"
 )
 
 func timestampToTime(timeStamp *string, local bool) (time.Time, error) {
