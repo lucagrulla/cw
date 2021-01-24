9dd90eff7023d6f791d98bd3cbdb56496d53d8f2
Luca Grulla <luca.grulla@gmail.com>
2021-01-24 18:12:52 +0000

    add autocomplete files

---
+++ cw.bash
@@ -0,0 +1,9 @@
+_cw_bash_autocomplete() {
+    local cur prev opts base
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    opts=$( ${COMP_WORDS[0]} --completion-bash ${COMP_WORDS[@]:1:$COMP_CWORD} )
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+}
+complete -F _cw_bash_autocomplete cw
---
+++ cw.zsh
@@ -0,0 +1,13 @@
+#compdef cw
+autoload -U compinit && compinit
+autoload -U bashcompinit && bashcompinit
+
+_cw_bash_autocomplete() {
+    local cur prev opts base
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    opts=$( ${COMP_WORDS[0]} --completion-bash ${COMP_WORDS[@]:1:$COMP_CWORD} )
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+}
+complete -F _cw_bash_autocomplete cw
